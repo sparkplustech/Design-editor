74740f83c7768adc5ea1640640e3e3f2808c35e1
aniketnaik12 <aniket@sparkplustech.in>
2024-01-25 13:57:30 +0530

    replaced add file icon

--- src/components/common/FileUpload.js
+++ src/components/common/FileUpload.js
@@ -72,7 +72,7 @@ class FileUpload extends Component {
 		return (
 			<Dragger {...props}>
 				<p className="ant-upload-drag-icon">
-					<Icon type="inbox" />
+					<Icon type="file-add" />
 				</p>
 				<p className="ant-upload-text">Click or drag file to this area to upload</p>
 				<p className="ant-upload-hint">{`Support for a single upload. Limited to ${limit}MB or less`}</p>
